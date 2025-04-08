420bdaa9564552d56512bd1e528084b470a81254
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 15:13:57 -0300

    feat: inicial page

--- sched-system/src/app/page.tsx
+++ sched-system/src/app/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 
-export default function Home() {
+import AgendamentoList from "@/app/components/SchedList/SchedList";
+import Link from "next/link";
+
+export default function HomePage() {
   return (
-    <div>
-    </div>
+    <main className="max-w-2xl mx-auto py-10 px-4">
+      <h1 className="text-3xl font-bold mb-6">📅 Lista de Agendamentos</h1>
+
+      <AgendamentoList />
+
+      <div className="mt-6">
+        <Link href="@/app/pages/new/page.tsx" className="text-blue-600 hover:underline">
+          + Novo agendamento
+        </Link>
+      </div>
+    </main>
   );
 }
