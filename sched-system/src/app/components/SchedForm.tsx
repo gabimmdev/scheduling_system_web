08b1f31d1de1eb75a545733f76db6ad77c128376
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 12:50:30 -0300

    feat: structure folders/files

---
+++ sched-system/src/app/components/SchedForm.tsx
@@ -0,0 +1,6 @@
+export default function SchedForm() {
+    return (
+        <>
+        </>
+    )
+}
---
+++ sched-system/src/app/components/SchedList.tsx
@@ -0,0 +1,6 @@
+export default function SchedList() {
+    return (
+        <>
+        </>
+    )
+}
