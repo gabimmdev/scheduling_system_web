9d2144502725e12dc6b17cbfceaf6106401b592f
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-08 15:14:20 -0300

    style: update structure foulders

---
+++ sched-system/src/app/pages/new/page.tsx
@@ -0,0 +1,17 @@
+import SchedForm from "@/app/components/SchedForm/SchedForm";
+import Link from "next/link";
+
+
+export default function New() {
+    return (
+        <main className="max-w-xl mx-auto py-10">
+        <h1 className="text-2xl font-bold mb-6">Novo Agendamento</h1>
+        <SchedForm />
+        <div className="mt-6">
+          <Link href="/" className="text-blue-600 hover:underline">
+            ← Voltar para a lista
+          </Link>
+        </div>
+      </main>
+    );
+}
