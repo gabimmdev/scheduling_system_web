a0eda230dbd6e8168c90eb1f046953a7b2c2055d
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 12:05:07 -0300

    fix: build

--- sched-system/postcss.config.mjs
+++ sched-system/postcss.config.mjs
@@ -1,5 +1,13 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  // tailwind.config.js
+  theme: {
+    extend: {
+      fontFamily: {
+        'segoe': ['"Segoe UI"', 'sans-serif'],
+      },
+    },
+  },
 };
 
 export default config;
