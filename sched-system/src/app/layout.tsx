6def88c5be7282648ca9052b9981d6dd5f967517
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 12:05:41 -0300

    fix: import css and chnage language

--- sched-system/src/app/layout.tsx
+++ sched-system/src/app/layout.tsx
@@ -1,12 +1,12 @@
-
+import "./style/globals.css";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
+    <html lang="pt-br">
+      <body className="bg-gray-900 text-white" 
       >
         {children}
       </body>
