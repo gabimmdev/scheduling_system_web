80a6767e9112fd76f467acb5593e984e9ce59951
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 12:06:25 -0300

    fix: use client and button

--- sched-system/src/app/pages/home/Home.tsx
+++ sched-system/src/app/pages/home/Home.tsx
@@ -1,15 +1,16 @@
-"use client"
-import SchedList from "@/app/components/schedlist/SchedList";
+import SchedList from "@/app/components/SchedList/SchedList";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="max-w-2xl mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-6">Agendamentos</h1>
+      <h1 className="text-3xl font-segoe bold mb-6 ">Agendamentos</h1>
       <SchedList />
       <div className="mt-6">
-        <Link href="pages/new" className="text-blue-600 hover:underline">
+        <Link href="pages/new">
+        <button className="text-white border-1px bg-transparent hover:bg-green">
           + Novo agendamento
+          </button>
         </Link>
       </div>
     </main>
