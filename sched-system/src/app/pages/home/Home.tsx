a2276504d9ec16c606664d164609c7a22e47aae2
gabimmdev <gabrielagomescezar@gmail>
2025-04-09 09:40:38 -0300

    fix: rename list and add use client

--- sched-system/src/app/pages/home/Home.tsx
+++ sched-system/src/app/pages/home/Home.tsx
@@ -1,13 +1,14 @@
-import AgendamentoList from "@/app/components/SchedList/SchedList";
+"use client"
+import SchedList from "@/app/components/schedlist/SchedList";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="max-w-2xl mx-auto py-10">
       <h1 className="text-3xl font-bold mb-6">Agendamentos</h1>
-      <AgendamentoList />
+      <SchedList />
       <div className="mt-6">
-        <Link href="/novo" className="text-blue-600 hover:underline">
+        <Link href="pages/new" className="text-blue-600 hover:underline">
           + Novo agendamento
         </Link>
       </div>
