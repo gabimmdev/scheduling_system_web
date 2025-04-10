85c0f6148bb2de1ef03eae5d175fc3a689711e9f
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-10 10:14:57 -0300

    fix: button

--- sched-system/src/app/pages/agendamento/agendamento.tsx
+++ sched-system/src/app/pages/agendamento/agendamento.tsx
@@ -8,7 +8,7 @@ export default function Agendamento () {
         </div>
       <div className="mt-6">
         <Link href="pages/new">
-        <button className="text-white border-1px bg-transparent hover:bg-green">
+        <button className="text-white border-1px bg-transparent hover:bg-green cursor-pointer">
           + Novo agendamento
           </button>
         </Link>
