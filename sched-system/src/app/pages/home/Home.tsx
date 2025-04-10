83b930e1992ce0277f3dab0d21f73f52dcce4cdb
gabimmdev <gabrielagomescezar@gmail.com>
2025-04-09 22:57:34 -0300

    fix: div novo agendamento

--- sched-system/src/app/pages/home/Home.tsx
+++ sched-system/src/app/pages/home/Home.tsx
@@ -1,18 +1,11 @@
-import SchedList from "@/app/components/SchedList/SchedList";
-import Link from "next/link";
+
+import Agendamento from "../agendamento/agendamento";
 
 export default function Home() {
   return (
     <main className="max-w-2xl mx-auto py-10">
       <h1 className="text-3xl font-segoe bold mb-6 ">Agendamentos</h1>
-      <SchedList />
-      <div className="mt-6">
-        <Link href="pages/new">
-        <button className="text-white border-1px bg-transparent hover:bg-green">
-          + Novo agendamento
-          </button>
-        </Link>
-      </div>
+      <Agendamento />
     </main>
   );
 }
